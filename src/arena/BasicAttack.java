ea0c3b60a121393b3c259fe7d7571b641c27e506
Ss910 <125534060+Ss910@users.noreply.github.com>
2026-04-04 18:18:48 +0800

    Update BasicAttack.java

--- src/arena/BasicAttack.java
+++ src/arena/BasicAttack.java
@@ -1,7 +1,15 @@
 public class BasicAttack implements Action {
   @Override
-  public void execute(Combatant user, Combatant target, BattleEngine engine) {
-    int damage = Math.max(0, user.getAttack() = target.getDefense());
-    
+  public String execute(Combatant user, Combatant target, BattleEngine engine) {
+    int damage = Math.max(0, user.getAttack() - target.getDefense());
+    int initHp = target.getHp();
+    target.takeDamage(damage);
+
+    return String.format("%s -> %s -> %s: HP: %d -> %d (dmg: %d - %d = %d)", user.getName(), getName(), target.getName(), initHp, target.getHp(), user.getAttack(), target.getDefense(), damage);
+  }
+
+  @Override
+  public String getName() {
+      return "BasicAttack";
   }
 }
