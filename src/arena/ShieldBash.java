061669ec1f0f37c0c2cfca540d13fe64f2df6af1
Ss910 <125534060+Ss910@users.noreply.github.com>
2026-04-05 18:46:47 +0800

    Create ShieldBash.java

---
+++ src/arena/ShieldBash.java
@@ -0,0 +1,28 @@
+package arena;
+
+public class ShieldBash extends SpecialSkill {
+    @Override
+    public String execute(Combatant user, Combatant target, BattleEngine engine) {
+        if (!isUsable()) {
+            return "ShieldBash on cooldown!";
+        }
+
+        int damage = Math.max(0, user.getAttack() - target.getDefense());
+        int initHp = target.getHp();
+        target.takeDamage(damage);
+        target.applyEffect(new Stun());
+        triggerCoolDown();
+
+        if (target.getHp() > 0) {
+            return String.format("%s -> %s -> %s: HP: %d -> %d (dmg: %d - %d = %d) | %s STUNNED (%d turns)", user.getName(), getName(), target.getName(), initHp, target.getHp(), user.getAttack(), target.getDefense(), damage, target.getName(), getCurCoolDown());
+        }
+        else {
+            return String.format("%s -> %s -> %s: HP: %d -> %d ELIMINATED (dmg: %d - %d = %d)", user.getName(), getName(), target.getName(), initHp, target.getHp(), user.getAttack(), target.getDefense(), damage);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "ShieldBash";
+    }
+}
