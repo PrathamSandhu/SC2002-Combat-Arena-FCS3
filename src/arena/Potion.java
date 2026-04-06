be32ec80e2de2e469c50eff9b84bb4271ee8afc9
Ss910 <125534060+Ss910@users.noreply.github.com>
2026-04-06 15:51:10 +0800

    Create Potion.java

---
+++ src/arena/Potion.java
@@ -0,0 +1,17 @@
+package arena;
+
+public class Potion implements Item {
+    @Override
+    public String use(Combatant user, BattleEngine engine) {
+        int heal = 100;
+        int initHp = user.getHp();
+        user.heal(heal);
+
+        return String.format("%s -> Item -> %s used: HP: %d -> %d (+100)", user.getName(), getName(), initHp, user.getHp());
+    }
+
+    @Override
+    public String getName() {
+        return "Potion";
+    }
+}
