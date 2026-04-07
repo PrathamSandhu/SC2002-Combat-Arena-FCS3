04cc903364bf7fd086972715f4024944c833bddc
Ss910 <125534060+Ss910@users.noreply.github.com>
2026-04-07 22:34:00 +0800

    Update SmokeBomb.java

--- src/arena/SmokeBomb.java
+++ src/arena/SmokeBomb.java
@@ -3,10 +3,8 @@ package arena;
 public class SmokeBomb implements Item {
     @Override
     public String use(Player user, BattleEngine engine) {
-        SmokeBombEffect smokeBombEffect = new SmokeBombEffect();
-        smokeBombEffect.apply(user);
-        user.applyEffect(smokeBombEffect);
-        return String.format("%s -> Item -> %s used: damage taken = 0 (2 turns)", user.getName(), getName());
+        user.applyEffect(new SmokeBombEffect());
+        return String.format("%s used: damage taken = 0 (2 turns)", getName());
     }
 
     @Override
