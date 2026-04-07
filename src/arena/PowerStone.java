83f947a25bec2cf90ab4ab13e297e3cf47370c16
Ss910 <125534060+Ss910@users.noreply.github.com>
2026-04-07 17:15:38 +0800

    Update PowerStone.java

--- src/arena/PowerStone.java
+++ src/arena/PowerStone.java
@@ -2,7 +2,7 @@ package arena;
 
 public class PowerStone implements Item {
     @Override
-    public String use(Combatant user, BattleEngine engine) {
+    public String use(Player user, BattleEngine engine) {
         Action skill = user.getSpecialSkill();
         skill.execute(user, engine.selectTarget(), engine); // see engine code first
 
