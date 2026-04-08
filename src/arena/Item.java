9f13b61661150f3f4f9467dc7e960db8fdbe4bd8
shannonlim <shannon.lim.wernsze@gmail.com>
2026-04-08 21:31:10 +0800

    Updated Item.java

--- src/arena/Item.java
+++ src/arena/Item.java
@@ -1,5 +1,7 @@
-public Interface Item {
-  void use(Player user, BattleEngine engine);
+package arena;
+
+public interface Item {
+  String use(Player user, BattleEngine engine);
 
   String getName();
 }
