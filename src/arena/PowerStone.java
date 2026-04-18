0bd648688adb5f04478e279fbfaf01204e0c7168
richardreinhart28 <richard007@e.ntu.edu.sg>
2026-04-18 21:35:47 +0800

    Update PowerStone.java

--- src/arena/PowerStone.java
+++ src/arena/PowerStone.java
@@ -4,15 +4,12 @@ public class PowerStone implements Item {
     @Override
     public String use(Player user, BattleEngine engine) {
         SpecialSkill skill = user.getSpecialSkill();
-        int preTurnCoolDown = engine.getPreTurnCoolDown();
-        Combatant target= engine.selectTarget(engine.getEnemies());
+        int before = skill.getCurCoolDown();
+        String result = skill.execute(user, engine.selectTarget(engine.getEnemies()), engine);
+        skill.curCoolDown = before;
 
-        skill.resetCoolDown(0);
-        String result = skill.execute(user, target, engine);
-        skill.resetCoolDown(preTurnCoolDown + 1);;
-        
-        return String.format("%s used -> %s | Cooldown unchanged -> %d (Power Stone does not affect cooldown) | Power Stone consumed",
-        		getName(), result, preTurnCoolDown);
+        return String.format("%s used: %s | Cooldown unchanged -> %d (%s does not affect cooldown) | %s consumed", getName(), result,
+                            before, getName(), getName());
     }
 
     @Override
