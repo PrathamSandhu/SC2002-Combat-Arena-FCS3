60834aba80cbca1c4490250461a7fc4d5f275ed0
richardreinhart28 <richard007@e.ntu.edu.sg>
2026-04-18 21:08:32 +0800

    Update Wolf.java

--- src/arena/Wolf.java
+++ src/arena/Wolf.java
@@ -2,11 +2,19 @@ package arena;
 
 public class Wolf extends Enemy
 {
+	private final String name;
+	
     public Wolf()
     {
         super(40, 45, 5, 35);
+        this.name = "Wolf";
     }
-
+    
+    public Wolf(String name)
+    {
+        super(40, 45, 5, 35);
+        this.name = name;
+    }
     @Override
     public String getName()
     {
